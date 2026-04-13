e381b62f5fe6ac8db1ebaddcfd8f716a5b83b8c1
dreamboy001226 <dreamboy001226@gmail.com>
2026-04-13 12:56:45 +0500

    add S20 N20 support

--- build-y2q.sh
+++ build-y2q.sh
@@ -8,13 +8,13 @@ NC='\033[0m'
 
 
 # ===== AnyKernel3 =====
-AK3_REPO="https://github.com/skye-tachyon/AnyKernel3"
-AK3_BRANCH="r8q"
+AK3_REPO="https://github.com/dreamboy001226/AnyKernel3.git"
+AK3_BRANCH="y2q"
 AK3_DIR="$(pwd)/android/AnyKernel3"
 
-ZIPNAME="not-CI-$(date '+%Y%m%d').zip"
+ZIPNAME="Astro_Core-$(date '+%Y%m%d').zip"
 TC_DIR="$(pwd)/tc/clang-r522817"
-DEFCONFIG="vendor/kona-not_defconfig vendor/samsung/kona-sec-not.config vendor/samsung/r8q.config"
+DEFCONFIG="vendor/kona-not_defconfig vendor/samsung/kona-sec-not.config vendor/samsung/y2q.config"
 
 OUT_DIR="$(pwd)/out"
 BOOT_DIR="$OUT_DIR/arch/arm64/boot"
@@ -22,7 +22,7 @@ DTS_DIR="$BOOT_DIR/dts/vendor/qcom"
 
 if test -z "$(git rev-parse --show-cdup 2>/dev/null)" &&
    head=$(git rev-parse --verify HEAD 2>/dev/null); then
-    ZIPNAME="${ZIPNAME::-4}-$(echo $head | cut -c1-8)-r8q.zip"
+    ZIPNAME="${ZIPNAME::-4}-$(echo $head | cut -c1-8)-y2q.zip"
 fi
 
 export PATH="$TC_DIR/bin:$PATH"
@@ -78,7 +78,7 @@ else
 fi
 
 rm -rf AnyKernel3
-echo "[*] Cloning AnyKernel3 for r8q"
+echo "[*] Cloning AnyKernel3 for y2q"
 git clone -q -b "$AK3_BRANCH" "$AK3_REPO" AnyKernel3 || exit 1
 
 echo -e "Preparing zip...\n"
